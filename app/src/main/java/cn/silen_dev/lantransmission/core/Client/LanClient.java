2309787863f76aa7004f9ffbf17d0267067d53d7
SilenCN <1039795376@qq.com>
2018-04-15 15:28:29 +0800

    主界面基本框架

---
+++ app/src/main/java/cn/silen_dev/lantransmission/core/Client/LanClient.java
@@ -0,0 +1,10 @@
+package cn.silen_dev.lantransmission.core.Client;
+
+public class LanClient {
+    public void start(){
+
+    }
+    public void stop(){
+
+    }
+}
---
+++ app/src/main/java/cn/silen_dev/lantransmission/core/Server/LanServer.java
@@ -0,0 +1,13 @@
+package cn.silen_dev.lantransmission.core.Server;
+
+public class LanServer {
+    public void start(){
+
+    }
+    public void stop(){
+
+    }
+    public boolean getStatus(){
+        return false;
+    }
+}
